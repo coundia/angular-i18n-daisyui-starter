f90e838e0508b033e253d268111c42e5f2f342ca
pcoundia <papacoundia@gmail.com>
2025-05-24 14:13:48 +0000

    [wip]

--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -1,28 +1,47 @@
 import { inject, Injectable, signal } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import {Category} from '../models/category.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Category } from '../models/category.model';
+import {AuthService} from '../../../shared/security/services/auth.service';
+import {API_BASE} from '../../../shared/constantes/shared-imports';
 
 @Injectable({ providedIn: 'root' })
 export class CategoryService {
   private readonly http = inject(HttpClient);
-  private readonly base = 'http://127.0.0.1:8095/api/v1';
+  private readonly auth = inject(AuthService);
+  private readonly base = API_BASE+'/v1';
 
   readonly categories = signal<Category[]>([]);
 
+  private headers(): HttpHeaders {
+    const token = this.auth.token();
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    });
+  }
+
   fetchAll() {
-    this.http.get<{ content: Category[] }>(`${this.base}/queries/categorys?page=0&limit=10`)
-      .subscribe(res => this.categories.set(res.content));
+    this.http.get<{ content: Category[] }>(
+      `${this.base}/queries/categorys?page=0&limit=10`,
+      { headers: this.headers() }
+    ).subscribe(res => this.categories.set(res.content));
   }
 
   create(dto: Partial<Category>) {
-    return this.http.post(`${this.base}/commands/category`, dto);
+    return this.http.post(`${this.base}/commands/category`, dto, {
+      headers: this.headers(),
+    });
   }
 
   update(id: string, dto: Partial<Category>) {
-    return this.http.put(`${this.base}/commands/category/${id}`, dto);
+    return this.http.put(`${this.base}/commands/category/${id}`, dto, {
+      headers: this.headers(),
+    });
   }
 
   delete(id: string) {
-    return this.http.delete(`${this.base}/commands/category/${id}`);
+    return this.http.delete(`${this.base}/commands/category/${id}`, {
+      headers: this.headers(),
+    });
   }
 }
